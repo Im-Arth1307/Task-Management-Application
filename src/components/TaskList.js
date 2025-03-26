af205cdeee401afd5891ab4da5ce891b51f59fcf
Atharva Jakhetiya <jakhetiyaatharva@gmail.com>
2025-03-26 15:32:22 +0530

    added some animations

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,30 +1,39 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleTask, deleteTask } from "../redux/taskSlice";
+import { motion, AnimatePresence } from "framer-motion";
 import "./TaskList.css";
 
 const TaskList = () => {
-  const tasks = useSelector((state) => state.tasks || []);// Get tasks from Redux store
+  const tasks = useSelector((state) => state.tasks);
   const dispatch = useDispatch();
-
   return (
-    <div className="task-container">
-      <h2>My Tasks</h2>
-      <ul>
+    <ul className="task-list">
+      <AnimatePresence>
         {tasks.map((task) => (
-          <li key={task.id} className={task.completed ? "completed" : ""}>
-            <input
-              type="checkbox"
-              checked={task.completed}
-              onChange={() => dispatch(toggleTask(task.id))}
+          <motion.li
+            key={task.id}
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            className="task-item"
+          >
+            <input 
+              type="checkbox" 
+              checked={task.completed} 
+              onChange={() => dispatch(toggleTask(task.id))} 
             />
-            {task.text}
-            <button onClick={() => dispatch(deleteTask(task.id))}>❌</button>
-          </li>
+            <span className={task.completed ? "completed" : ""}>
+              {task.text}
+            </span>
+            <button className="delete-btn" onClick={() => dispatch(deleteTask(task.id))}>
+              ❌
+            </button>
+          </motion.li>
         ))}
-      </ul>
-    </div>
+      </AnimatePresence>
+    </ul>
   );
-};
+  };
 
 export default TaskList;
