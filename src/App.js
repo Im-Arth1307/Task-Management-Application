60fcfce9bc7d3dcafcff67721355a2b2a3042a1c
Atharva Jakhetiya <jakhetiyaatharva@gmail.com>
2025-03-26 15:21:02 +0530

    created App.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
+import { useDispatch } from "react-redux";
+import { addTask } from "./redux/taskSlice";
+import "./App.css";
+
 
 const App = () => {
-    return (
-      <div>
-        <h1>Task Manager App</h1>
-        <TaskForm />
-        <TaskList />
-      </div>
-    );
+  const [taskText, setTaskText] = useState("");
+  const dispatch = useDispatch();
+
+  const handleAddTask = () => {
+    if (taskText.trim() !== "") {
+      dispatch(addTask(taskText));
+      setTaskText("");
+    }
   };
+
+  return (
+    <div className="app-container">
+      <h1>Task Manager App</h1>
+
+      <div className="task-input-container">
+        <input
+          type="text"
+          value={taskText}
+          onChange={(e) => setTaskText(e.target.value)}
+          placeholder="Add a new task..."
+        />
+        <button className="add-task-button" onClick={handleAddTask}>
+          Add Task
+        </button>
+      </div>
+
+      <TaskList />
+    </div>
+  );
+};
 
 export default App;
